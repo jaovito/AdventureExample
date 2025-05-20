a7c026d9570fdaf0b9154a4940bce938a084f44b
jaovito <junior.vitorio.dias@gmail.com>
2025-05-19 21:46:48 -0300

    FEAT: Add base cover system

---
+++ Source/AdventureGame/Private/Components/CoverComponent.cpp
@@ -0,0 +1,94 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/CoverComponent.h"
+#include "DrawDebugHelpers.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values for this component's properties
+UCoverComponent::UCoverComponent()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+    CurrentState = ECoverState::None;
+}
+
+
+void UCoverComponent::BeginPlay()
+{
+    Super::BeginPlay();
+    OwnerCharacter = Cast<ACharacter>(GetOwner());
+}
+
+
+void UCoverComponent::TryEnterCover() 
+{
+    if (CurrentState != ECoverState::None)
+        return;
+
+	FVector Location;
+    FVector Normal;
+
+	if (FindCover(Location, Normal))
+    {
+        CoverLocation = Location;
+        CoverNormal = Normal;
+        AlignToCover();
+        CurrentState = ECoverState::InCover;
+
+	    // get character movement
+
+		UCharacterMovementComponent* OwnerMovement = OwnerCharacter->GetCharacterMovement();
+
+		OwnerMovement->SetMovementMode(MOVE_None);
+		// OwnerMovement->PlayCoverMontage(); // opcional
+	}
+}
+
+void UCoverComponent::ExitCover()
+{
+    if (CurrentState == ECoverState::InCover)
+    {
+        CurrentState = ECoverState::None;
+		UCharacterMovementComponent* OwnerMovement = OwnerCharacter->GetCharacterMovement();
+        OwnerMovement->SetMovementMode(EMovementMode::MOVE_Walking);
+        // OwnerCharacter->PlayCoverExitMontage(); // opcional
+    }
+}
+
+bool UCoverComponent::FindCover(FVector& OutLocation, FVector& OutNormal)
+{
+    FVector Start = OwnerCharacter->GetActorLocation();
+    FVector Forward = OwnerCharacter->GetActorForwardVector();
+    FVector End = Start + Forward * 150.0f;
+
+    FHitResult Hit;
+    FCollisionQueryParams Params;
+    Params.AddIgnoredActor(OwnerCharacter);
+
+    bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params);
+
+    if (bHit && Hit.bBlockingHit)
+    {
+        OutLocation = Hit.ImpactPoint;
+        OutNormal = Hit.ImpactNormal;
+        return true;
+    }
+
+    return false;
+}
+
+void UCoverComponent::AlignToCover()
+{
+    FVector Forward = -CoverNormal; // olhando para a parede
+    FRotator TargetRot = Forward.Rotation();
+
+    OwnerCharacter->SetActorLocation(CoverLocation - Forward * 50.0f); // ajusta distância
+    OwnerCharacter->SetActorRotation(TargetRot);
+}
+
+bool UCoverComponent::IsInCover()
+{
+    return CurrentState == ECoverState::InCover;
+}
---
+++ Source/AdventureGame/Public/Components/CoverComponent.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animations/Enums/ECoverState.h"
+#include "GameFramework/Character.h"
+#include "Components/ActorComponent.h"
+#include "CoverComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ADVENTUREGAME_API UCoverComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UCoverComponent();
+
+	UFUNCTION(BlueprintCallable, Category = "Cover")
+	void TryEnterCover();
+
+	UFUNCTION(BlueprintCallable, Category = "Cover")	
+	void ExitCover();
+
+	void MoveAlongCover(float Value);
+
+	UFUNCTION(BlueprintPure, Category = "Cover")
+	bool IsInCover();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	ECoverState CurrentState;
+
+	FVector CoverLocation;
+	FVector CoverNormal;
+
+	ACharacter* OwnerCharacter;
+
+	void AlignToCover();
+	bool FindCover(FVector& OutLocation, FVector& OutNormal);
+};
